64ce3360aef131a9a0caaa9b803a1ac7f4b555b4
Krish <krish1604mehta@gmail.com>
2025-06-06 15:49:45 +0530

    link change

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Sidebar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-64 bg-[#1A1A1A] text-[#39ff14] p-4 rounded-r-[20px] shadow-lg z-40">
-      <h2 className="text-2xl font-bold mb-4">Sidebar</h2>
+      <Link href={"/"}>
+        <h2 className="text-2xl font-bold mb-4">Sidebar</h2>
+      </Link>
       <ul className="space-y-2">
         <li>
           <Link href="/dashboard" className="hover:text-white">Home</Link>
