13d09eb77e4bcf844e31dc02ace06ff4f0a84492
Krish <krish1604mehta@gmail.com>
2025-06-06 15:44:57 +0530

    seo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Dashboard from "./dashboard/page";
 
+export const generateMetadata = () => ({
+  title: "Home - User Dashboard",
+  description: "This is the home page of your app.",
+});
+
 export default function Home() {
   return (
     <div>
